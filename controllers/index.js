458633e8b6540d4c37db2528b361ebec7be846e4
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2019-04-28 14:15:09 -0700

    query fixed

--- controllers/index.js
+++ controllers/index.js
@@ -12,23 +12,22 @@ exports.register = async function(request, response, db) {
     } else {
       const password = crypto.createHash('md5').update(request.body.password).digest("hex")
       const dob = request.body.dob
-      await db.execute('INSERT INTO users SET username = ?, password = ?, dob = ?', [username, password, dob])
 
-      // create auth token for the user registered
-      const [user, field] = await db.execute('SELECT id, username, password FROM users WHERE username = ?', [username]);
+      const insertedUser = await db.execute('INSERT INTO users SET username = ?, password = ?, dob = ?', [username, password, dob])
+      // get user id of the user registered
+      const userId = insertedUser[0].insertId
 
-      //check if passwords match
-      if (user[0].password === password) {
-          // create a token
-          const token = jwt.sign({id: user[0].id}, config.secret, {
+      if (userId) {
+          // create auth token
+          const token = jwt.sign({id: userId}, config.secret, {
               expiresIn: 600 // expires in 10 minutes
           })
-          await db.execute('UPDATE users SET auth_token = ? WHERE id = ?', [token, user[0].id])
+          await db.execute('UPDATE users SET auth_token = ? WHERE id = ?', [token, userId])
           msg = 'User created'
 
           return response.status(200).json({"authToken": token})
       } else {
-          return response.status(200).json({"Error": "Incorrect password."})
+          return response.status(200).json({"Error": "Username not found"})
       }
     }
     return response.status(200).json({"Message": msg})
