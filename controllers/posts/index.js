ac771c74aec985397d0ba02277fc3dc4772d8e06
alialavi <ali.alavi201@gmail.com>
2019-05-09 19:23:45 -0700

    Fetch all posts or single by uuid

--- controllers/posts/index.js
+++ controllers/posts/index.js
@@ -23,3 +23,28 @@ exports.create = async function(request, response, db) {
         return response.status(500).json({"Error": "Unexpected error occured. Please try again in a while"})
     }
   }
+
+exports.get = async function(request, response, db) {
+    try {
+        const userId = await userController.getUserByToken(request.body.authToken, db)
+        if(!userId) return response.status(400).json({"Error": "Invalid auth token."})
+
+        let whereStr = ''
+        let qvals = []
+
+        if(request.body.uuid) {
+            whereStr = ' WHERE uuid = ?'
+            qvals.push(request.body.uuid)
+        }
+        const [posts, fields] = await db.execute(`
+            SELECT u.id, u.username, u.image_url, p.uuid, p.text, p.created_at 
+            FROM users u
+            JOIN posts p on p.user_id = u.id ${whereStr}`, qvals
+        ) 
+        return response.status(200).json({"posts": posts})
+    } 
+    catch(err) {
+        console.log(err)
+        return response.status(500).json({"Error": "Unexpected error occured. Please try again in a while"})
+    }
+}
