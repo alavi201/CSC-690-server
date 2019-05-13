5c07b565d60e6004214b076cf2e045adb0e52cc1
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2019-05-12 23:22:26 -0700

    search response modified

--- controllers/search/index.js
+++ controllers/search/index.js
@@ -20,16 +20,8 @@ exports.search = async function(request, response, db) {
             WHERE username like ?`, [userId, queryVal]
         ) 
         results['users'] = users
-
-        const [posts, postFields] = await db.execute(`
-            SELECT u.id, u.username, u.image_url, p.uuid, p.text, p.created_at 
-            FROM users u
-            JOIN posts p on p.user_id = u.id
-            AND p.text like ? 
-            ORDER BY p.id DESC`, [queryVal]
-        )
-        results['posts'] = posts
-        return response.status(200).json({"results": results})
+        
+        return response.status(200).json(results)
     } 
     catch(err) {
         console.log(err)
